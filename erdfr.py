d18c4d491c42c6cfe25780dfaac790fec147ec65
S.Senojan <135141304+senojan@users.noreply.github.com>
2023-06-04 11:51:04 +0530

    Add files via upload

---
+++ erdfr.py
@@ -0,0 +1,16 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/HP/AppData/Local/Programs/Python/Python311/my first program.py
+Hello,world!
+>>> print('Hello,world!')
+Hello,world!
+>>> person=input('what is your name?')
+what is your name?senojan
+>>> print('Hello',person)
+Hello senojan
+>>> 
+>>> age=12
+>>> print(age)
+12
+>>> Weight=25
